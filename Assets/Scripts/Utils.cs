a55ce3cb69da7920b3b37b5bb0ff6004ba9fbe94
Michael Cox <31874317+Arylos07@users.noreply.github.com>
2021-09-05 10:22:08 -0500

    Added an automated version check
    The canvas now has a component that checks Modheim's version on launch by checking it against the redirect URL of the Github latest release URL. If a difference is detected, it opens Github.

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System;
 
 public static class Utils
 {
@@ -31,6 +32,33 @@ public static class UIUtils
         // (backwards loop because Destroy changes childCount)
         for (int i = parent.childCount - 1; i >= amount; --i)
             GameObject.Destroy(parent.GetChild(i).gameObject);
+    }
+
+    /// <summary>
+    /// takes a substring between two anchor strings (or the end of the string if that anchor is null)
+    /// </summary>
+    /// <param name="this">a string</param>
+    /// <param name="from">an optional string to search after</param>
+    /// <param name="until">an optional string to search before</param>
+    /// <param name="comparison">an optional comparison for the search</param>
+    /// <returns>a substring based on the search</returns>
+    public static string Substring(this string @this, string from = null, string until = null, StringComparison comparison = StringComparison.InvariantCulture)
+    {
+        var fromLength = (from ?? string.Empty).Length;
+        var startIndex = !string.IsNullOrEmpty(from)
+            ? @this.IndexOf(from, comparison) + fromLength
+            : 0;
+
+        if (startIndex < fromLength) { throw new ArgumentException("from: Failed to find an instance of the first anchor"); }
+
+        var endIndex = !string.IsNullOrEmpty(until)
+        ? @this.IndexOf(until, startIndex, comparison)
+        : @this.Length;
+
+        if (endIndex < 0) { throw new ArgumentException("until: Failed to find an instance of the last anchor"); }
+
+        var subString = @this.Substring(startIndex, endIndex - startIndex);
+        return subString;
     }
 
     // find out if any input is currently active by using Selectable.all
